503c36c2ee41d54c76f920cc0d68ce0d9ceb1c4b
Sarah Bulle <43357106+sbulle21@users.noreply.github.com>
2018-09-26 22:30:46 -0700

    Update TaskScheduler.java
    I have edited the Task Scheduler file to include GUI information. Feel free to take a look at it!

--- TaskScheduler.java
+++ TaskScheduler.java
@@ -1,20 +1,28 @@
-// CSE 360 Fall 2018
+//CSE 360 Fall 2018
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class TaskScheduler extends JApplet {
-	// Code for making GUI Java Applet
+	
+	private int APPLET_WIDTH = 800, APPLET_HEIGHT = 300;
+
 	private JTabbedPane tPane;
-   	private AddPanel addPanel;
-   	private ViewPanel viewPanel;
-	private ArrayList taskList;
+	private AddPanel addPanel;
+	private ViewPanel viewPanel;
+	private ArrayList<Task> taskList;
 
 	public void init() {
-		taskList = new ArrayList();
-		// Code for initializing GUI
-		addPanel = new AddPanel();
-		viewPanel = new ViewPanel();
+		taskList = new ArrayList<Task>();
+
+		viewPanel = new ViewPanel(taskList);
+		addPanel = new AddPanel(taskList, viewPanel);
+
+		tPane = new JTabbedPane();
+		tPane.addTab("Create Task", addPanel);
+		tPane.addTab("View Tasks", viewPanel);
+		getContentPane().add(tPane);
+		setSize (APPLET_WIDTH, APPLET_HEIGHT);
 	}
 }
