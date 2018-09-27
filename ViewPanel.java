f282dfbebda3496c872c61ded07c0604eef1fe5e
Sarah Bulle <43357106+sbulle21@users.noreply.github.com>
2018-09-26 22:34:13 -0700

    Update ViewPanel.java
    I have updated the View Panel file to include GUI information. Unfortunately, I have not yet set up the Clear Tasks button to actually do anything yet. Also, I have not tested yet if the text field displays anything because the Add Panel is currently impossible to use.

--- ViewPanel.java
+++ ViewPanel.java
@@ -1,15 +1,40 @@
-// CSE 360 Fall 2018
+//CSE 360 Fall 2018
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
 
-public class ViewPanel {
-	private ArrayList taskList;
-	
-	public ViewPanel(ArrayList taskList) {
+public class ViewPanel extends JPanel {
+	private ArrayList<Task> taskList;
+	private JTextArea allTasks;
+	private JPanel viewTasks;
+	private JButton reset;
+
+	public ViewPanel(ArrayList<Task> taskList) {
 		this.taskList = taskList;
-		// Other GUI data to put on view panel
+		
+		setLayout(new BorderLayout());
+
+		viewTasks = new JPanel(new GridLayout(1, 3));
+		reset = new JButton("Clear Tasks");
+		viewTasks.add(new JLabel(""));
+		viewTasks.add(reset);
+		viewTasks.add(new JLabel(""));
+		reset.addActionListener(new ButtonListener());
+
+		allTasks = new JTextArea(15,30);
+		allTasks.setEditable(false);
+		// Tasks are listed as strings in text area
+		JScrollPane scroll = new JScrollPane(allTasks);
+		add(scroll, BorderLayout.NORTH);
+		add(viewTasks, BorderLayout.SOUTH);
+	}
+
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			// Checks to see if there are any tasks and deletes them if there are
+			// Clears the text area
+		}
 	}
 }
