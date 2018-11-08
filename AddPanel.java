d41a63867e297581eb6ccb5fdc67082783e8e7b5
Sarah Bulle <43357106+sbulle21@users.noreply.github.com>
2018-11-08 15:26:44 -0700

    Update AddPanel.java
    I moved all of the UI elements to the one panel, hopefully this will allow us to reduce the complexity of our code.

--- AddPanel.java
+++ AddPanel.java
@@ -7,49 +7,86 @@ import java.util.*;
 import java.lang.*;
 
 public class AddPanel extends JPanel {
-	private JButton addTask;
+	private JButton addTask, processPaths, savePaths, resetTasks, updateTask;
 	private ViewPanel viewPanel;
 	private JLabel errorMessage;
 	private JTextField name, duration, dependencies;
 
 	public AddPanel(ViewPanel viewPanel) {
 		this.viewPanel = viewPanel;
+		JPanel wholePanel = new JPanel(new BorderLayout());
 		
 		JPanel createPanel = new JPanel(new BorderLayout());
-
-		JPanel welcome = new JPanel(new GridLayout(1, 1));
+		JPanel processPanel = new JPanel(new BorderLayout());
+		
+		JPanel welcome = new JPanel(new GridLayout(2, 1));
 		errorMessage = new JLabel("");
-		welcome.add(new JLabel("                                                                         Welcome to Task Scheduler"));
+		welcome.add(new JLabel("Welcome to Task Scheduler"));
+		welcome.add(new JLabel(""));
 		welcome.add(errorMessage);
 
-		JPanel fields = new JPanel(new GridLayout(10,1));
-		JPanel labels = new JPanel(new GridLayout(10,1));
+		JPanel addTitle = new JPanel(new GridLayout(2, 1));
+		addTitle.add(new JLabel("Create or Modify Tasks"));
+		
+		JPanel processTitle = new JPanel(new GridLayout(2, 1));
+		processTitle.add(new JLabel("Process and Save Paths"));
+		
+		JPanel taskOutput = new JPanel(new GridLayout(10, 1));
+		taskOutput.add(new JLabel("Tasks:"));
+		JPanel pathOutput = new JPanel(new GridLayout(10, 1));
+		pathOutput.add(new JLabel("Paths:"));
+		JPanel outputButtons = new JPanel(new GridLayout(1, 5));
+		processPaths = new JButton("Process");
+		savePaths = new JButton("Save");
+		resetTasks = new JButton("Reset");
+		outputButtons.add(new JLabel(""));
+		outputButtons.add(processPaths);
+		outputButtons.add(new JLabel(""));
+		outputButtons.add(savePaths);
+		outputButtons.add(new JLabel (""));
+		outputButtons.add(resetTasks);
+		outputButtons.add(new JLabel(""));
+		
+		processPanel.add(processTitle, BorderLayout.NORTH);
+		processPanel.add(taskOutput, BorderLayout.WEST);
+		processPanel.add(pathOutput, BorderLayout.EAST);
+		processPanel.add(outputButtons, BorderLayout.SOUTH);
+		
+		JPanel fields = new JPanel(new GridLayout(8,1));
+		JPanel labels = new JPanel(new GridLayout(8,1));
 
-		name = new JTextField(30);
-		duration = new JTextField(30);
-		dependencies = new JTextField(30);
+		name = new JTextField(10);
+		duration = new JTextField(10);
+		dependencies = new JTextField(10);
 
-		labels.add(new JLabel("           Enter Name of the Task"));
+		labels.add(new JLabel("Enter Name of Task"));
 		fields.add(name);
-		labels.add(new JLabel("           Enter Duration of the Task"));
+		labels.add(new JLabel("Enter Duration"));
 		fields.add(duration);
-		labels.add(new JLabel("           Enter any Dependencies the Task Has"));
+		labels.add(new JLabel("Enter any Dependencies"));
 		fields.add(dependencies);
 
-		JPanel buttons = new JPanel(new GridLayout(1, 3));
+		JPanel buttons = new JPanel(new GridLayout(1, 1));
 		addTask = new JButton("Add Task");
 		buttons.add(new JLabel(""));
 		buttons.add(addTask);
+		updateTask = new JButton("Update");
+		buttons.add(new JLabel(""));
+		buttons.add(updateTask);
 		buttons.add(new JLabel(""));
 		addTask.addActionListener(new ButtonListener());
 
-		createPanel.add(welcome, BorderLayout.NORTH);
+		createPanel.add(addTitle, BorderLayout.NORTH);
 		createPanel.add(labels, BorderLayout.WEST);
 		createPanel.add(fields, BorderLayout.EAST);
 		createPanel.add(buttons, BorderLayout.SOUTH);
-
+		
+		wholePanel.add(welcome, BorderLayout.NORTH);
+		wholePanel.add(createPanel, BorderLayout.WEST);
+		wholePanel.add(processPanel, BorderLayout.EAST);
+		
 		setLayout(new BorderLayout());
-		add(createPanel, BorderLayout.WEST);
+		add(wholePanel, BorderLayout.WEST);
 	}
 
 private class ButtonListener implements ActionListener {
