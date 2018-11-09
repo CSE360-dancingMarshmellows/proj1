7cdc91677b62064815dc81ccd580fe543935cc93
Sarah Bulle <43357106+sbulle21@users.noreply.github.com>
2018-11-09 10:03:30 -0700

    Update TaskScheduler.java
    Here is the updated Task Scheduler

--- TaskScheduler.java
+++ TaskScheduler.java
@@ -6,11 +6,10 @@ import java.util.ArrayList;
 
 public class TaskScheduler extends JApplet {
 	
-	private int APPLET_WIDTH = 800, APPLET_HEIGHT = 300;
+	private int APPLET_WIDTH = 1400, APPLET_HEIGHT = 300;
 
 	private JTabbedPane tPane;
 	private AddPanel addPanel;
-	private ViewPanel viewPanel;
 	private HelpPanel helpPanel;
 	private AboutPanel aboutPanel;
 	private ArrayList<Task> taskList;
@@ -18,17 +17,16 @@ public class TaskScheduler extends JApplet {
 	public void init() {
 		taskList = new ArrayList<Task>();
 
-		viewPanel = new ViewPanel(taskList);
-		addPanel = new AddPanel(viewPanel);
+		addPanel = new AddPanel();
 		helpPanel = new HelpPanel();
 		aboutPanel = new AboutPanel();
 
 		tPane = new JTabbedPane();
 		tPane.addTab("Create Task", addPanel);
-		tPane.addTab("View Tasks", viewPanel);
 		tPane.addTab("Help", helpPanel);
 		tPane.addTab("About", aboutPanel);
 		getContentPane().add(tPane);
 		setSize (APPLET_WIDTH, APPLET_HEIGHT);
 	}
 }
+
