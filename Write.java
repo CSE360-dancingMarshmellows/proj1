878bbc2729008ee4d957736d13d126fa0d58caa4
Sarah Bulle <43357106+sbulle21@users.noreply.github.com>
2018-11-16 13:19:23 -0700

    Update Write.java

--- Write.java
+++ Write.java
@@ -1,5 +1,5 @@
 // CSE 360 Fall 2018
-
+package old;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,16 +11,33 @@ import java.time.format.DateTimeFormatter;
 
 public class Write {
 	
-	public Write(String pathInfo, String fileName) {
-		File file = new File("./" + fileName + ".txt");
+	public Write(ArrayList<String> pathInfo, ArrayList<String> taskInfo, String fileName, int paths, int tasks) {
+		File file = new File("C:\\Users\\Sarah Bulle\\Desktop\\" + fileName + ".txt");
 		FileWriter fr = null;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime current = LocalDateTime.now();
 		try {
 			fr = new FileWriter(file);
-			fr.write("Task Scheduler Report:\n\n");
-			fr.write("Date and time of Creation: " + formatter.format(current) + "\n\n");
-			fr.write(pathInfo);
+			fr.write("Task Scheduler Report:");
+			fr.write(System.getProperty("line.separator"));
+			fr.write("Date and time of Creation: " + formatter.format(current));
+			fr.write(System.getProperty("line.separator"));
+			fr.write("Tasks:");
+			fr.write(System.getProperty("line.separator"));
+			int i = 0;
+			while (i < taskInfo.size()) {
+				fr.write(taskInfo.get(i));
+				fr.write(System.getProperty("line.separator"));
+				i++;
+			}
+			fr.write("Paths:");
+			fr.write(System.getProperty("line.separator"));
+			i = 0;
+			while (i < pathInfo.size()) {
+				fr.write(pathInfo.get(i));
+				fr.write(System.getProperty("line.separator"));
+				i++;
+			}
 		}
 		catch (IOException e) {
 			e.printStackTrace();
